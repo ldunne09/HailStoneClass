353533ee5f7a9b247933c9cd09150e49633a9bea
Logan <logandunne0927@gmail.com>
2021-04-05 17:27:06 -0600

    Add project files.

---
+++ HailStoneClass/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HailStoneClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*for(int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine($"args[{i}]={args[i]}");
+            }*/
+            foreach(var arg in args)
+               System.Console.WriteLine($"arg is {arg}");
+            
+
+           var hs = new HailStoneClass(7);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
